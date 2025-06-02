cd740ee01e2595f0dd784ef9c9e754fc83504f55
Aman <162727802+verma2510@users.noreply.github.com>
2025-06-02 21:28:51 +0530

    tictactoe 9 boxes added

--- src/components/TicTacToe.tsx
+++ src/components/TicTacToe.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import Block from "./Block";
 
 const TicTacToe: React.FC = () => {
-     return(
-        <div>
-           <Block/> 
-        </div>
-     )
-}
+  const size = 3;
+
+  return (
+    <div>
+      {Array.from({ length: size * size }).map((_, i) => (
+        <Block key={i} />
+      ))}
+    </div>
+  );
+};
 
 export default TicTacToe;
