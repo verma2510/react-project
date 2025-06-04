82ec253b2f320d12968c03455ceefa6bf0f2ec24
Aman <162727802+verma2510@users.noreply.github.com>
2025-06-04 23:45:55 +0530

    used DRY method for form

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,35 +1,39 @@
 import React from "react";
 
 const Player: React.FC = () => {
-    return (
-      <div className="border p-4">
-        <form action="" className="flex flex-col">
-          <label htmlFor="igname">Player 1: </label>
-          <input
-            type="text"
-            className="border rounded-md px-2 py-1 mb-2"
-            name="igname"
-            placeholder="your in-game name"
-          />
-          <label htmlFor="igname">Player 2: </label>
-          <input
-            type="text"
-            className="border rounded-md px-2 py-1 mb-2"
-            name="igname"
-            placeholder="your in-game name"
-          />
+  const players = [
+    { id: 1, label: "Player 1", name: "player1" },
+    { id: 2, label: "Player 2", name: "player2" },
+  ]; 
 
-          <label htmlFor="symbol">Choose your symbol: </label>
-          <div className="border">
-            {["X", "O"].map((symbol, index) => (
-              <button key={index} className="border px-2 py-1 w-[100px] font-bold text-lg">
-                {symbol}
-              </button>
-            ))}
+  return (
+    <div className="border p-4">
+      <form action="" className="flex flex-col">
+        {players.map((player) => (
+          <div key={player.id}>
+            <label htmlFor={player.name}>{player.label}: </label>
+            <input
+              type="text"
+              className="border rounded-md px-2 py-1 mb-2"
+              name={player.name}
+              placeholder="your in-game name"
+            />
           </div>
-        </form>
-      </div>
-    );
+        ))}
+        <label htmlFor="symbol">Choose your symbol: </label>
+        <div className="flex items-center justify-center gap-3 mt-1">
+          {["X", "O"].map((symbol, index) => (
+            <button
+              key={index}
+              className="border px-2 py-1 w-[75px] font-bold rounded-md text-lg cursor-pointer"
+            >
+              {symbol}
+            </button>
+          ))}
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Player;
