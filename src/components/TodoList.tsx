6805b8b660d69d30d103e45837a83da0476d70f4
Aman <162727802+verma2510@users.noreply.github.com>
2025-06-10 23:43:59 +0530

    list bg added (css)

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -8,8 +8,8 @@ interface TodoListProps{
 
 const TodoList: React.FC<TodoListProps> = (props) => {
     return(
-        <div className="flex items-center gap-2">
-            <input type="checkbox" className="appearance-none h-5 w-5 rounded-full border-2 border-gray-400 checked:border-blue-400 checked:bg-blue-400"/>
+        <div className="flex items-center bg-blue-200 px-2 rounded-md gap-2">
+            <input type="checkbox" className="appearance-none h-5 w-5 rounded-full border-2 bg-white border-gray-400 checked:border-blue-400 checked:bg-blue-400"/>
             <span className="flex-1 text-left py-1">{props.listName}</span>
             <span onClick={props.onClick} className="cursor-pointer text-xl bg-red-600 text-white rounded-md"><RxCross2/></span>
         </div>
